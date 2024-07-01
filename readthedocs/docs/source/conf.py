c70a4a5bdaf639f1e42d7b4ba5611ed7184e17e5
Sara Walton <spwalto@sandia.gov>
2024-07-01 13:53:56 -0600

    fixing yaml file

--- readthedocs/docs/source/conf.py
+++ readthedocs/docs/source/conf.py
@@ -22,6 +22,7 @@ extensions = [
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "ovis-hpc": ("https://ovis-hpc.readthedocs.io/en/latest/", None),
 }
 intersphinx_disabled_domains = ['std']
 
